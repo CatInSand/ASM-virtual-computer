973e091f7b43b7f5a52b9c6a855984c530fb1e0d
FinnVerbeeck <239849759+CatInSand@users.noreply.github.com>
2025-12-17 18:15:22 +0100

    Removed old commented out ROM

--- ASM_VM/main.cpp
+++ ASM_VM/main.cpp
@@ -7,42 +7,6 @@ int main()
 		Parser::ParseTokensToROM("program.s")
 	};
 
-	//const std::array<uint8_t, Computer::ROM_SIZE> ROM{
-	//	0x09, 0x05, //	LDA #$05
-	//	0x01, 0x04, //	CMP #$04
-	//	0x04, 0x0a, //	BMI :+
-	//	0x03, 0x0a, //	BEQ :+
-	//	0x06, 0x0b, //		JSR print_a_twenty_times
-	//				//	:
-	//	0x00,		//	BRK
-	//				// 
-	//				//	print_a_twenty_times:
-	//	0x11,		//		PHA
-	//	0x13,		//		PHX
-	//				//		
-	//	0x0b, 0x00,	//		LDX #$00
-	//				//	@loop:
-	//	0x09, 0x41, //		LDA #'A'
-	//	0x0f, 0xfe, //		STA $FE
-	//				// 
-	//	0x1d,		//		INX
-	//	0x11,		//		PHA
-	//	0x0e,		//		TXA
-	//	0x01, 0x14,	//		CMP #$14
-	//	0x12,		//		PLA
-	//	0x03, 0x1d,	//		BEQ :+
-	//	0x05, 0x0f,	//			JMP @loop
-	//				//		:
-	//				// 
-	//	0x09, 0x11,	//		LDA #$11
-	//	0x0f, 0xff,	//		STA $FF
-	//				//		
-	//	0x14,		//		PLX
-	//	0x12,		//		PLA
-	//	0x08,		//		RTS
-	//				//
-	//};
-
 	Computer computer{ ROM };
 	computer.Run();
 }
